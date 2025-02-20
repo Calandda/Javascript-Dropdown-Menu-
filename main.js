b96c97763564e9a386844c3e58275d320b94589e
Calandda <A.calandda@gmail.com>
2025-02-20 19:24:30 +0800

    finished

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import {test} from './dropdown/dropdown.js'
+function openDropdown(dropdownLink,dropdownItemLink){
+    const dropdown = document.querySelector(dropdownLink);
+    const dropdownItem = document.querySelector(dropdownItemLink);
 
-console.log(test);
+    dropdown.addEventListener("click",(e)=>{
+        if(dropdownItem.style.display === 'none' || !dropdownItem.style.display){
+            dropdownItem.style.display = 'grid';
+        } else if (dropdownItem.style.display === 'grid'){
+            dropdownItem.style.display = 'none';
+        }
+    })
+};
+
+openDropdown('.dropdownItem','.dropdownItemList');
